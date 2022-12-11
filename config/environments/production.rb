944e91051fcc050576d30c6b8620c6d688784f92
Kelvin M Matheka <beingmatheka@gmail.com>
2022-12-11 15:30:44 +0300

    reverting changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  # config.hosts << "coffee-shop-production.up.railway.app" 
-  config.hosts << "rails-production-f085.up.railway.app"
+  config.hosts << "coffee-shop-production.up.railway.app" 
 
   # Code is not reloaded between requests.
   config.cache_classes = true
