f681468616b0ff587d0122e333e213391a208d99
Kelvin M Matheka <beingmatheka@gmail.com>
2022-12-11 15:24:21 +0300

    deployment configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "<Coffee-Shop-Project-production.up.railway.app" 
+  # config.hosts << "coffee-shop-production.up.railway.app" 
+  config.hosts << "rails-production-f085.up.railway.app"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
